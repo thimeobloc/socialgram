a45fe103b48cab8826a776bfac7e773c8c421bce
Antonin-code <antoninmaubert224@gmail.com>
2026-09-08 10:48:42 +0200

    add registry routes system + finished registry front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+import RegistryPages from "./pages/registryPages";
+
 
 export default function App() {
   return (
     <Routes>
+      <Route path="/register" element={<RegistryPages />} />
     </Routes>
   );
 }
